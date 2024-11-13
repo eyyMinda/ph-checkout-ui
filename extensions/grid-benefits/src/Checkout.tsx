e4c13b5d417f21b54b3063a9e539f4b4ef324254
Mindaugas Straksys <gamerx14x@gmail.com>
2024-11-13 13:18:44 +0200

    edit: implemented dynamic sizes & added 2 more blocks for grid-benefits

--- extensions/grid-benefits/src/Checkout.tsx
+++ extensions/grid-benefits/src/Checkout.tsx
@@ -7,9 +7,7 @@ import {
   Grid,
   GridItemSize,
   TextBlock,
-  BlockStack,
-  BlockLayout,
-  InlineStack
+  BlockLayout
 } from "@shopify/ui-extensions-react/checkout";
 
 export default reactExtension("purchase.checkout.block.render", () => <Extension />);
@@ -21,26 +19,45 @@ interface settings {
   text_2?: string;
   source_3?: string;
   text_3?: string;
+  source_4?: string;
+  text_4?: string;
+  source_5?: string;
+  text_5?: string;
   padding_block?: Spacing;
   padding_inline?: Spacing;
 }
 
 function Extension() {
-  const { source_1, text_1, source_2, text_2, source_3, text_3, padding_block, padding_inline }: settings =
-    useSettings();
+  const {
+    source_1,
+    text_1,
+    source_2,
+    text_2,
+    source_3,
+    text_3,
+    source_4,
+    text_4,
+    source_5,
+    text_5,
+    padding_block,
+    padding_inline
+  }: settings = useSettings();
 
   const items = [
     { source: source_1, text: text_1 },
     { source: source_2, text: text_2 },
-    { source: source_3, text: text_3 }
-  ].filter(item => item.source && item.text); // Filter out invalid entries
-
+    { source: source_3, text: text_3 },
+    { source: source_4, text: text_4 },
+    { source: source_5, text: text_5 }
+  ].filter(item => item.source && item.text);
+  const size = sizes[items.length];
   const dynamicColumns = items.map(() => "1fr" as GridItemSize);
+
   return (
     <Grid
       columns={dynamicColumns}
       rows={"auto"}
-      spacing={"tight"}
+      spacing={size.spacing}
       background={"subdued"}
       border={"base"}
       borderRadius={"loose"}
@@ -49,12 +66,12 @@ function Extension() {
         <BlockLayout
           key={index}
           inlineAlignment={"center"}
-          spacing={"tight"}
+          spacing={size.spacing}
           padding={["base", "none", "none", "none"]}>
-          <View maxInlineSize={60} maxBlockSize={60}>
+          <View maxInlineSize={size.img} maxBlockSize={size.img}>
             <Image fit="cover" source={item.source} />
           </View>
-          <TextBlock size="base" appearance={"accent"} inlineAlignment={"center"}>
+          <TextBlock size={size.text} appearance={"accent"} inlineAlignment={"center"}>
             {item.text}
           </TextBlock>
         </BlockLayout>
@@ -62,3 +79,11 @@ function Extension() {
     </Grid>
   );
 }
+
+const sizes = {
+  "1": { img: 100, text: "large", spacing: "tight" },
+  "2": { img: 80, text: "medium", spacing: "tight" },
+  "3": { img: 60, text: "base", spacing: "tight" },
+  "4": { img: 40, text: "small", spacing: "extraTight" },
+  "5": { img: 30, text: "extraSmall", spacing: "none" }
+};
