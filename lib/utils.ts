c528d2044480d818acae8550abf84e260ee67215
Mindaugas Straksys <gamerx14x@gmail.com>
2024-11-13 15:17:21 +0200

    moved: utils & helpers to lib/utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
 import { Style, Display, MaybeResponsiveConditionalStyle } from "@shopify/ui-extensions-react/checkout";
+import { CHECKOUT_SCREEN_BREAKPOINT } from "./constants";
 
 export const displayDesktopStyle: MaybeResponsiveConditionalStyle<Display> = Style.default<Display>("none").when(
   { viewportInlineSize: { min: "medium" } },
@@ -8,3 +9,14 @@ export const displayMobileStyle: MaybeResponsiveConditionalStyle<Display> = Styl
   { viewportInlineSize: { min: "medium" } },
   "none"
 );
+
+// Debounce utility
+export function debounce(func: () => void, wait: number): () => void {
+  let timeout: ReturnType<typeof setTimeout>;
+  return () => {
+    clearTimeout(timeout);
+    timeout = setTimeout(func, wait);
+  };
+}
+export const normalizeImageSize = (size: number | undefined, defaultSize: number = 80): number =>
+  size && size >= 10 ? size : defaultSize;
